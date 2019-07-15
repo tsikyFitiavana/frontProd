d911aa2858b61fe7b91dd37f3358b32b3133a485
tsikyFitiavana <tsikybr@gmail.com>
2019-07-15 10:45:58 +0300

    prodfront v1

---
+++ build/precache-manifest.fbbdcb4eb872bc9a5328919b6d1e37c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb78e5fbece7f496c7a3f1d128ec3d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f42275146f7f30c2e4",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "12f42275146f7f30c2e4",
+    "url": "/static/js/main.0d94cf85.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
